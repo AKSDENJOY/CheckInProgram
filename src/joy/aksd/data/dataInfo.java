87ecb479d63aec25dc23644c1ec14798390505a4
EnjoyD <wy277853573@163.om>
2018-01-11 18:02:02 +0800

    checkInProgram v0.1

---
+++ src/joy/aksd/data/dataInfo.java
@@ -0,0 +1,149 @@
+package joy.aksd.data;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.security.MessageDigest;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.LinkedBlockingQueue;
+
+
+/**
+ * Created by EnjoyD on 2017/4/18.
+ */
+public class dataInfo {
+    private dataInfo(){}
+
+    /**
+     * 缓存区块
+     */
+    public static final LinkedList<Block> blocks= new LinkedList<>();
+    /**
+     * 验证完脚本后的纪录池
+     */
+    public static LinkedBlockingQueue<Record> effectiveRecord = new LinkedBlockingQueue<>();
+    /**
+     * 验证完时间与顺序戳后的未使用纪录池，类似于 未使用的UTXO
+     */
+    public static ConcurrentHashMap<String,Record> freshRecord =new ConcurrentHashMap<>();
+    /**
+     * 验证完成后的记录池，用以创建默克尔树。
+     */
+    public static ArrayList<Record> identifedRecord=new ArrayList<>();
+    /**
+     * 打包生成区块时保存的记录
+     */
+    public static ArrayList<Record> unPackageRecord=new ArrayList<>();
+
+    /**
+     * timeRecord  时间纪录，用以存储adjustCount个区块时间
+     * adjustCount 需要多少个区块进行一次难度调整
+     */
+    public static ArrayList<Integer> timeRecord=new ArrayList<>();
+    public static int adjustCount=10;
+    /**
+     * 区块能包含的最大条目数
+     */
+    public static int merkleTreeLimitation=64;
+
+    public static MessageDigest SHA256x;
+
+    /**
+     *  exceptTime 目标时间，期望平均多长时间产生一个区块，proof of work中为定值。
+     *  errorTime 时间误差，误差范围内时间不变
+     */
+//    public static final int exceptTime=1920;//单位：秒
+    public static final int exceptTime=600;//单位：秒
+    public static final int errorTime=50;//单位：秒
+    public static final int TTL=5;
+    /**
+     * 区块存储位置
+     */
+    public static final String location="blockTest";
+    /**
+     * 区块索引
+     */
+    public static ArrayList<Long> indexBlock=new ArrayList<>();
+    /**
+     * 缓存区块数量
+     */
+    public static final int cacheBlockCount=1000;
+    /**
+     * 核心线程，用来进行pow 广播 写入硬盘的顺序执行，将其线程模块化用以同步block时进行
+     */
+    public static ExecutorService coreWork=Executors.newSingleThreadExecutor();
+    /**
+     * pow中断信号
+     */
+    public static boolean interupt =false;
+    /**
+     * 期待的块号
+     */
+    public static int num=0;
+
+    public static final String ECNAME="secp160r1";
+
+    public static final int PORT=49999;
+
+    public static String ROOTIP="";
+    public static final String IPLOCATION="ip.txt";
+    public static String localIp;
+
+    public static HashSet<String> IPList= new HashSet<>();
+    static {
+        try {
+            setIP();
+        } catch (IOException e) {
+            System.out.println("IP设置error");
+            System.exit(1);
+        }
+    }
+
+    private static void setIP() throws IOException {
+        BufferedReader reader=new BufferedReader(new FileReader(IPLOCATION));
+        String IP=reader.readLine();
+        ArrayList<String> ipList=new ArrayList<>();
+        while (true){
+            String temIP=reader.readLine();
+            if (temIP==null)
+                break;
+            ipList.add(temIP);
+        }
+        reader.close();
+        if (checkIP(IP))
+            ROOTIP=IP;
+        else
+            throw new IOException();
+        Iterator<String> it=ipList.iterator();
+        while (it.hasNext()){
+            String temIP=it.next();
+            if (!checkIP(temIP))
+                it.remove();
+        }
+        IPList.add(ROOTIP);
+        IPList.addAll(ipList);
+    }
+
+    public static boolean checkIP(String ip) {
+        return true;
+    }
+
+
+    public static int getUnixTime(){
+        return (int) (System.currentTimeMillis()/1000);
+    }
+
+    public static void interuptCoreThread(){
+        interupt=true;
+    }
+    public static void interuptReset(){
+        interupt=false;
+    }
+
+}
---
+++ src/user/querySelfRecord.java
@@ -0,0 +1,81 @@
+package user;
+
+import joy.aksd.data.Record;
+
+import java.io.*;
+import java.net.Socket;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+import static joy.aksd.data.dataInfo.PORT;
+import static joy.aksd.data.dataInfo.ROOTIP;
+import static joy.aksd.data.protocolInfo.SELFQUERY;
+import static joy.aksd.tools.readAndPrintData.printRecord;
+import static joy.aksd.tools.toByte.hexStringToByteArray;
+import static joy.aksd.tools.toInt.byteToInt;
+
+/**
+ * Created by EnjoyD on 2017/5/15.
+ */
+public class querySelfRecord {
+
+    public void querySelfRecord(){
+        //get lockScript
+        byte[] lockScript = null;
+        try {
+            lockScript=getLockScript();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        //start query
+        try {
+            startQuery(lockScript);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private void startQuery(byte[] lockScript) throws IOException {
+        Socket socket=new Socket(ROOTIP,PORT);
+        InputStream in=socket.getInputStream();
+        OutputStream out=socket.getOutputStream();
+        out.write(SELFQUERY);
+        out.write(lockScript);
+        try {
+            while (true) {
+                byte[] tem = new byte[2];
+                in.read(tem);
+                tem = new byte[byteToInt(tem)];
+                in.read(tem);
+                Record record=new Record(tem);
+                printRecord(record);
+            }
+        }catch (Exception e){
+            ;
+        }
+        return;
+    }
+
+    private byte[] getLockScript() throws IOException, NoSuchAlgorithmException {
+        DataInputStream in = new DataInputStream(new FileInputStream("./key"));
+        in.readUTF();
+        String second = in.readUTF();
+        String third = in.readUTF();
+        in.close();
+        byte []x=hexStringToByteArray(second);
+        byte []y=hexStringToByteArray(third);
+        byte[] result = new byte[x.length + y.length];
+        System.arraycopy(x, 0, result, 0, x.length);
+        System.arraycopy(y, 0, result, x.length, y.length);
+        return MessageDigest.getInstance("SHA-256").digest(result);
+    }
+
+    public static void main(String[] args) {
+        new querySelfRecord().querySelfRecord();
+    }
+
+
+}
